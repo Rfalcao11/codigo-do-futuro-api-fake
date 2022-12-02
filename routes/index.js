de17cd883ea60b0498ba6955fe39441f3b602c8b
Rfalcao11 <rafael_lencina@yahoo.com.br>
2022-12-02 12:53:01 -0300

    api fake json

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const HomeController = require("../controllers/homeController")
+const HomeController = require("../controllers/homeController");
+const usuariosController = require('../controllers/usuariosController');
+
 
 
 router.get('/', HomeController.index) ;
+router.get('/usuarios', usuariosController.index) ;
 
 module.exports = router;
